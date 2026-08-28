687b2364dc932b194aa2b3e512667c829eb99241
Douglas Carvalho <douglasc.dev@gmail.com>
2026-08-28 19:34:31 -0300

    repo

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Flatpak development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              flatpak
+              flatpak-builder
+              wget
+              ostree
+            ];
+          };
+        }
+      );
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          import-bundle = pkgs.writeShellScriptBin "import-bundle" ''
+            VERSION=0.1.21-alpha
+            wget https://github.com/NuvioMedia/NuvioDesktop/releases/download/$VERSION/Nuvio-Linux-x86_64-$VERSION.flatpak
+            flatpak build-import-bundle repo Nuvio-Linux-x86_64-$VERSION.flatpak
+            ostree summary --repo=repo --update
+          '';
+          default = self.packages.${system}.import-bundle;
+        }
+      );
+    };
+}
