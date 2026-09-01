262c0d8b605ee3d98b6c5c2ad0d68eb2e98fb067
Douglas Carvalho <douglasc.dev@gmail.com>
2026-09-01 09:26:27 -0300

    add new command to .nix

--- flake.nix
+++ flake.nix
@@ -42,6 +42,7 @@
             VERSION=0.1.22-alpha
             wget https://github.com/NuvioMedia/NuvioDesktop/releases/download/$VERSION/Nuvio-Linux-x86_64-$VERSION.flatpak
             flatpak build-import-bundle repo Nuvio-Linux-x86_64-$VERSION.flatpak
+            flatpak build-update-repo repo
             ostree summary --repo=repo --update
           '';
           default = self.packages.${system}.import-bundle;
