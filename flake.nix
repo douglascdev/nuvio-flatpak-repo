3d2898bbf130eaca3cc75f1930cd43a4ae3f40c7
Douglas Carvalho <douglasc.dev@gmail.com>
2026-09-01 09:39:04 -0300

    Revert "update to 0.1.12-alpha"
    This reverts commit 46de88793c9ffb202334348c8c88629135965ed3.

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
         in
         {
           import-bundle = pkgs.writeShellScriptBin "import-bundle" ''
-            VERSION=0.1.22-alpha
+            VERSION=0.1.21-alpha
             wget https://github.com/NuvioMedia/NuvioDesktop/releases/download/$VERSION/Nuvio-Linux-x86_64-$VERSION.flatpak
             flatpak build-import-bundle repo Nuvio-Linux-x86_64-$VERSION.flatpak
             ostree summary --repo=repo --update
