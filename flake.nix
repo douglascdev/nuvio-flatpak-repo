be70fa94e151c30cb947ae1c9dfb2a72a424827b
Douglas Carvalho <douglasc.dev@gmail.com>
2026-09-10 17:28:10 -0300

    version 0.1.23-alpha

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
         in
         {
           import-bundle = pkgs.writeShellScriptBin "import-bundle" ''
-            VERSION=0.1.22-alpha
+            VERSION=0.1.23-alpha
             wget https://github.com/NuvioMedia/NuvioDesktop/releases/download/$VERSION/Nuvio-Linux-x86_64-$VERSION.flatpak
             flatpak build-import-bundle repo Nuvio-Linux-x86_64-$VERSION.flatpak
             flatpak build-update-repo --generate-static-deltas --prune repo
